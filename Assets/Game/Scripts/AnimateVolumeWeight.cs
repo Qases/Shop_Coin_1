7c1a22fc546310d5fc157fe49cc7eac2aec69846
Qases <ramik@2ch.hk>
2022-09-12 01:53:24 +0400

    Animate model, postfx, UV_Bags

---
+++ Assets/Game/Scripts/AnimateVolumeWeight.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace SortItems
+{
+    public class AnimateVolumeWeight : MonoBehaviour
+    {
+        [SerializeField] private Volume _volume;
+        [SerializeField] private float _animationTime = 0.3f;
+
+        private bool _isAnimate = false;
+        private float _elapsedTime;
+
+
+        public void Animate()
+        {
+            if (_isAnimate)
+            {
+                return;
+            }
+
+            _isAnimate = true;
+
+            _elapsedTime = 0f;
+
+            StartCoroutine(AnimateVolume());
+
+        }
+
+        protected IEnumerator AnimateVolume()
+        {
+            while (_elapsedTime < _animationTime)
+            {
+                yield return new WaitForEndOfFrame();
+
+                _volume.weight = Mathf.Sin(Mathf.PI * _elapsedTime / _animationTime);
+
+                _elapsedTime += Time.deltaTime;
+                
+            }
+
+            _isAnimate = false;
+        }
+
+    }
+}
